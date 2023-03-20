667f81554a73ff0494f5e22174d06b714248a06d
Code Sandbox <hello@codesandbox.io>
2023-03-20 23:00:13 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.7793c48e882fba31c06c8d435673b713.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2caf97963a2406b57c845b197c5bd093",
+    "url": "https://gladiator-20.github.io/csb-k4vnwe/index.html"
+  },
+  {
+    "revision": "01f4cb63f08d07d5cf66",
+    "url": "https://gladiator-20.github.io/csb-k4vnwe/static/js/2.4b0dee0d.chunk.js"
+  },
+  {
+    "revision": "f8480cca27316f450338",
+    "url": "https://gladiator-20.github.io/csb-k4vnwe/static/js/main.f2d0fb0b.chunk.js"
+  },
+  {
+    "revision": "016f72d71caed4c2780c",
+    "url": "https://gladiator-20.github.io/csb-k4vnwe/static/js/runtime-main.0a888ccb.js"
+  }
+]);
